6b08bb588dce67d5d41e8f7016a8af9e0a42f574
Mehedi Hasan Chonchol <chonchol.am@gmail.com>
2018-09-09 00:04:25 +0600

    Initial Solution Structure

---
+++ src/main/java/me/chonchol/pocketshop/PocketshopApplication.java
@@ -0,0 +1,12 @@
+package me.chonchol.pocketshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocketshopApplication.class, args);
+    }
+}
